382f4215e874cd475fca0c51d13ead2ca95260c5
am7850233-netizen <am7850233@gmail.com>
2026-07-16 11:42:54 +1000

    оформление заказа

--- script.js
+++ script.js
@@ -157,7 +157,106 @@ function removeItem(index){
     localStorage.setItem("cart",JSON.stringify(cart));
 
     renderCart();
+}
+    
+const checkoutButton = document.getElementById("checkoutButton");
+
+const orderModal = document.getElementById("orderModal");
+const closeModal = document.getElementById("closeModal");
+const sendOrder = document.getElementById("sendOrder");
+
+if (checkoutButton) {
+
+    checkoutButton.addEventListener("click", function () {
+
+        const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+        if (cart.length === 0) {
+            alert("Корзина пуста!");
+            return;
+        }
+
+        orderModal.style.display = "flex";
+
+    });
 
 }
+
+closeModal.addEventListener("click", () => {
+
+    orderModal.style.display = "none";
+
+});
+
+sendOrder.addEventListener("click", function () {
+
+    const name = document.getElementById("customerName").value;
+    const phone = document.getElementById("customerPhone").value;
+    const telegram = document.getElementById("customerTelegram").value;
+    const city = document.getElementById("customerCity").value;
+    const comment = document.getElementById("customerComment").value;
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    let message = "🛒 Новый заказ!\n\n";
+
+    message += "👤 Имя: " + name + "\n";
+    message += "📞 Телефон: " + phone + "\n";
+    message += "💬 Telegram: " + telegram + "\n";
+    message += "🏙 Город: " + city + "\n";
+
+    if(comment){
+        message += "📝 Комментарий: " + comment + "\n";
+    }
+
+    message += "\n📦 Товары:\n";
+
+    let total = 0;
+
+    cart.forEach(item => {
+
+        message += `${item.name}\n${item.quantity} шт. × ${item.price} ₽\n\n`;
+
+        total += item.price * item.quantity;
+
+    });
+
+    message += "💰 Итого: " + total + " ₽";
+
+    fetch("https://api.telegram.org/bot8706367967:AAGmehwgHy2MPxMcrZHlD-evPyNhGT2B__8/sendMessage", {
+
+        method: "POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+
+            chat_id:"1934343117",
+
+            text:message
+
+        })
+
+    })
+
+    .then(() => {
+
+        alert("Спасибо за заказ!");
+
+        localStorage.removeItem("cart");
+
+        orderModal.style.display = "none";
+
+        renderCart();
+
+    });
+
+});
+
+
+        
+
 
    
